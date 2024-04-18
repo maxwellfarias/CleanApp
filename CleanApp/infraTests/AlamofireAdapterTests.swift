202571309c0a9db0bf19cad6a2348dc3fa18344f
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-04-18 00:58:54 -0300

    move duplicated code to a helper to improve readability

--- CleanApp/infraTests/AlamofireAdapterTests.swift
+++ CleanApp/infraTests/AlamofireAdapterTests.swift
@@ -16,7 +16,7 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL, with data: Data?) {
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
@@ -24,19 +24,38 @@ class AlamofireAdapter {
 final class AlamofireAdapterTests: XCTestCase {
     func test_post_should_make_request_with_valid_url_and_method()  {
         let url = makeUrl()
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [UrlProtocolStub.self]
-        let session = Session(configuration: configuration)
-        let sut = AlamofireAdapter(session: session)
-        let exp = expectation(description: "waiting")
-        UrlProtocolStub.observeRequest { request in
+        testRequestFor(url: url, data: makeValidData()) { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual(request.method, .post)
 //As the httpBody for some reason is returning null, so it was necessary to get the httpBodyStream. The check carried out is just to see if there is data, but the ideal scenario would be to convert httpBodyStream to Data and compare the input and output data.
             XCTAssertNotNil(request.httpBodyStream)
+        }
+    }
+    
+    func test_post_should_make_request_with_no_data()  {
+        testRequestFor(data: nil) { request in
+            XCTAssertNil(request.httpBodyStream)
+        }
+    }
+}
+
+extension AlamofireAdapterTests {
+    func makeSut() -> AlamofireAdapter {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        return sut
+    }
+    
+    func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
+        let sut = makeSut()
+        sut.post(to: url, with: data)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            action(request)
             exp.fulfill()
         }
-        sut.post(to: url, with: makeValidData())
         wait(for: [exp], timeout: 1)
     }
 }
