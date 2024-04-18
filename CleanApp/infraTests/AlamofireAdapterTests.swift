91199e127743c0e376161df39c361be4487dfe5d
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-04-18 11:00:39 -0300

    ensure AlamofireAdapter completes with erro when request completes with error

--- CleanApp/infraTests/AlamofireAdapterTests.swift
+++ CleanApp/infraTests/AlamofireAdapterTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import Alamofire
+import Data
 
 class AlamofireAdapter {
     private let session: Session
@@ -15,9 +16,13 @@ class AlamofireAdapter {
         self.session = session
     }
     
-    func post(to url: URL, with data: Data?) {
-        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
-        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
+    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data, HttpError>) -> Void) {
+        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData { dataResponse in
+            switch dataResponse.result {
+            case .failure: completion(.failure(.noConnectivity))
+            case .success: break
+            }
+        }
     }
 }
 
@@ -27,7 +32,7 @@ final class AlamofireAdapterTests: XCTestCase {
         testRequestFor(url: url, data: makeValidData()) { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual(request.method, .post)
-//As the httpBody for some reason is returning null, so it was necessary to get the httpBodyStream. The check carried out is just to see if there is data, but the ideal scenario would be to convert httpBodyStream to Data and compare the input and output data.
+            //As the httpBody for some reason is returning null, so it was necessary to get the httpBodyStream. The check carried out is just to see if there is data, but the ideal scenario would be to convert httpBodyStream to Data and compare the input and output data.
             XCTAssertNotNil(request.httpBodyStream)
         }
     }
@@ -37,20 +42,35 @@ final class AlamofireAdapterTests: XCTestCase {
             XCTAssertNil(request.httpBodyStream)
         }
     }
+    
+    func test_post_should_complete_with_error_when_request_completes_with_error() {
+        let sut = makeSut()
+        UrlProtocolStub.simulate(data: nil, response: nil, error: makeError())
+        let exp = expectation(description: "waiting")
+        sut.post(to: makeUrl(), with: makeValidData()) { result in
+            switch result {
+            case .success(let data): XCTFail("Expected error got \(result) instead")
+            case .failure(let error): XCTAssertEqual(error, .noConnectivity)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 extension AlamofireAdapterTests {
-    func makeSut() -> AlamofireAdapter {
+    func makeSut(file: StaticString = #file, line: UInt = #line) -> AlamofireAdapter {
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
         let sut = AlamofireAdapter(session: session)
+        checkMemoryLeak(for: sut, file: file, line: line)
         return sut
     }
     
     func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
         let sut = makeSut()
-        sut.post(to: url, with: data)
+        sut.post(to: url, with: data) { _ in }
         let exp = expectation(description: "waiting")
         UrlProtocolStub.observeRequest { request in
             action(request)
@@ -62,9 +82,18 @@ extension AlamofireAdapterTests {
 
 class UrlProtocolStub: URLProtocol {
     static var emit: ((URLRequest) -> Void)?
+    static var data: Data?
+    static var response: HTTPURLResponse?
+    static var error: Error?
     
     static func observeRequest(completion: @escaping (URLRequest) -> Void) {
         UrlProtocolStub.emit = completion
+    }
+    
+    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        UrlProtocolStub.data = data
+        UrlProtocolStub.response = response
+        UrlProtocolStub.error = error
     }
     
     override  open class func canInit(with request: URLRequest) -> Bool {
@@ -74,9 +103,21 @@ class UrlProtocolStub: URLProtocol {
     override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
-
+    
     open override func startLoading() {
         UrlProtocolStub.emit?(request)
+//        The following methods prepare data, response and error information that will later be sent to the class that made the request.
+        if let data = UrlProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let response = UrlProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        if let error = UrlProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+//        Shoot the response
+        client?.urlProtocolDidFinishLoading(self)
     }
     
     open override func stopLoading() {}
