92ec3e4352df472047efbf4d5a3441303313703d
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-04-24 15:06:03 -0300

    Add presentation layer

---
+++ CleanApp/PresentationTests/PresentationTests.swift
@@ -0,0 +1,16 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Maxwell Farias on 24/04/24.
+//
+
+import XCTest
+@testable import Presentation
+
+final class PresentationTests: XCTestCase {
+
+    func testExample() throws {
+    }
+
+}
